ab920c4ebf1ada4da45dce4583438e8dd4c5550b
Mike8888888 <98763935+Mike8888888@users.noreply.github.com>
2023-11-24 23:58:58 +1100

    Update gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import path from 'node:path';
 import parseToJson from './parseToJson.js';
 import compareJsons from './compareJsons.js';
 
@@ -17,10 +16,6 @@ program
     const file1 = parseToJson(filepath1);
     const file2 = parseToJson(filepath2);
     console.log(compareJsons(file1, file2));
-    /*console.log(typeof file1);
-    console.log(file2);
-    console.log(path.resolve(process.cwd(), filepath1));
-    console.log(path.resolve(filepath1));*/
   });
 
 program.parse();
