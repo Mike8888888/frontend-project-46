e411b587f58d045c1e4328efe7d66cda321a8600
Михаил Клименко <zero1986@inbox.ru>
2024-11-29 20:51:44 +1100

    Minor changes in tests and added stylish formatter as default

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,25 +1,19 @@
 /* eslint-disable no-undef */
-/* eslint-disable quote-props */
-import path from 'node:path';
-import fs from 'node:fs';
-import process from 'node:process';
-import yaml from 'js-yaml';
-import gendiff from '../src/gendiff.js';
-
-const readFile = (filePath) => JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf-8'));
-const readYaml = (filePath) => yaml.load(fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8'));
+import parser from '../src/parser.js';
+import stylish from '../src/formatters/stylish.js';
+import compareJsons from '../src/compareJsons.js';
 
 const correctResult = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}';
 
-const json1 = readFile('__fixtures__/file1.json');
-const json2 = readFile('__fixtures__/file2.json');
-const yaml1 = readYaml('__fixtures__/file1.yml');
-const yaml2 = readYaml('__fixtures__/file2.yml');
+const json1 = parser('__fixtures__/file1.json');
+const json2 = parser('__fixtures__/file2.json');
+const yaml1 = parser('__fixtures__/file1.yml');
+const yaml2 = parser('__fixtures__/file2.yml');
 
 test('Different JSONs', () => {
-  expect(gendiff(json1, json2)).toEqual(correctResult);
+  expect(stylish(compareJsons(json1, json2))).toEqual(correctResult);
 });
 
 test('YAMLs', () => {
-  expect(gendiff(yaml1, yaml2)).toEqual(correctResult);
+  expect(stylish(compareJsons(yaml1, yaml2))).toEqual(correctResult);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import parseToObject from './parser.js';
 import compareJsons from './compareJsons.js';
 import stylish from './formatters/stylish.js';
-import fs from 'node:fs';
+import parser from './parser.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.8.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    const file1 = parseToObject(filepath1);
-    const file2 = parseToObject(filepath2);
-    const diff = compareJsons(file1, file2)
-    console.log(stylish(diff));
+  .action((filepath1, filepath2, options) => {
+    const obj1 = parser(filepath1);
+    const obj2 = parser(filepath2);
+    if (options.format === 'plain') {
+      console.log('plain formatter in progress');
+    } else {
+      console.log(stylish(compareJsons(obj1, obj2)));
+    }
+    console.log();
   });
 
 program.parse();
