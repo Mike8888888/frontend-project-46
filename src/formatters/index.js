6e3231d3787e9ce60db027bdbc7eba78b0e5908e
zero1986@inbox.ru <mvklime@DESKTOP-MG2CHR8.localdomain>
2024-12-27 07:04:37 +1100

    Remove let from index formatter and fix test for json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-else-return */
 import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
 export default (options, diff) => {
-  let result;
   if (options === 'plain') {
-    result = plain(diff);
+    return plain(diff);
   } else if (options === 'json') {
-    result = json(diff);
+    return json(diff);
   } else {
-    result = stylish(diff);
+    return stylish(diff);
   }
-  return result;
 };
