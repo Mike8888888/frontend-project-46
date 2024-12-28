04469e70250877c6df02d89af8b60779dbd43bac
zero1986@inbox.ru <mvklime@DESKTOP-MG2CHR8.localdomain>
2024-12-29 00:48:36 +1100

    Change compareJsons.js method from map to reduce, so no mutation now

--- src/compareJsons.js
+++ src/compareJsons.js
@@ -1,48 +1,66 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-else-return */
+/* eslint-disable no-undef */
 /* eslint-disable array-callback-return */
 import _ from 'lodash';
 
 function compareJsons(obj1, obj2) {
   const keys = Object.keys({ ...obj1, ...obj2 });
-  const result = [];
-  keys.map((key) => {
-    if (
-      Object.hasOwn(obj1, key)
-      && Object.hasOwn(obj2, key)
-      && _.isObject(obj1[key])
-      && _.isObject(obj2[key])) {
-      result.push({
-        key,
-        value: compareJsons(obj1[key], obj2[key]),
-        state: 'nested',
-      });
-    } else if (!Object.hasOwn(obj1, key)) {
-      result.push({
-        key,
-        value: obj2[key],
-        state: 'added',
-      });
-    } else if (!Object.hasOwn(obj2, key)) {
-      result.push({
-        key,
-        value: obj1[key],
-        state: 'deleted',
-      });
-    } else if (obj1[key] !== obj2[key]) {
-      result.push({
-        key,
-        value: obj1[key],
-        value2: obj2[key],
-        state: 'updated',
-      });
-    } else {
-      result.push({
-        key,
-        value: obj1[key],
-        state: 'unchanged',
-      });
-    }
-  });
-  return result;
+  function getAST(keys) {
+    const cb = (result, key) => {
+      if (
+        Object.hasOwn(obj1, key)
+        && Object.hasOwn(obj2, key)
+        && _.isObject(obj1[key])
+        && _.isObject(obj2[key])) {
+        return [
+          ...result, {
+            key,
+            value: compareJsons(obj1[key], obj2[key]),
+            state: 'nested',
+          },
+        ];
+      } else if (!Object.hasOwn(obj1, key)) {
+        return [
+          ...result, {
+            key,
+            value: obj2[key],
+            state: 'added',
+          },
+        ];
+      } else if (!Object.hasOwn(obj2, key)) {
+        return [
+          ...result, {
+            key,
+            value: obj1[key],
+            state: 'deleted',
+          },
+        ];
+      } else if (obj1[key] !== obj2[key]) {
+        return [
+          ...result, {
+            key,
+            value: obj1[key],
+            value2: obj2[key],
+            state: 'updated',
+          },
+        ];
+      } else {
+        return [
+          ...result, {
+            key,
+            value: obj1[key],
+            state: 'unchanged',
+          },
+        ];
+      }
+    };
+
+    const ast = keys.reduce(cb, []);
+    return ast;
+  }
+  return getAST(keys);
 }
 
 export default compareJsons;
